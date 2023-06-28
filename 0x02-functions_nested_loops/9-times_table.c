439224e7731e5aeabb75fc87ba3425ca65ce844d
Ogoodluckchisom <ogoodluckchisom@gmail.com>
2023-06-28 13:48:23 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,10 +5,10 @@
  * Description: It prints 9 times table starting with 0
  * Return: Nothing.
  */
-
 void times_table(void)
 {
 	int i, j, n;
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
